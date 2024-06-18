38457c96a39043011024e2d85123d64c959a8c49
joyline-cheruto <joycheruto832@gmail.com>
2024-06-18 18:16:39 +0300

    created a calculator to do simple arithmetic

---
+++ calc.py
@@ -0,0 +1,19 @@
+def calculator(number1, number2, operator):
+    match operator:
+        case "+":
+            return number1 + number2
+        case "-":
+            return number1 - number2
+        case "*":
+            return number1 * number2
+        case "/":
+            return number1 / number2
+        case "_":
+            return "Invalid operator!"
+
+
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+operator = input("Enter the operator (+, -, *, /): ")
+result = calculator(num1, num2, operator)
+print("Result:", result)
